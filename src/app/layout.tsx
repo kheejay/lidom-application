38d53372352e266ae22f205d77b4e26bc6ecb057
Mark Andrei Encanto <markandrei.encanto@wvsu.edu.ph>
2024-05-08 10:47:19 +0800

    updating app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import { GeistSans } from "geist/font/sans";
 import "./globals.css";
-import ThemeProvider from "@/context/ThemeContext";
+import { ThemeProvider } from "@/context/ThemeContext";
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
